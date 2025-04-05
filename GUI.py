298b024d57bd4b85ee091e6ddacac0609dcf9c74
__link__ <68714207+linkold@users.noreply.github.com>
2025-04-05 12:36:15 -0500

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -1,49 +1,42 @@
 import sys
-import os
-import webbrowser
-from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLabel, QStackedWidget, QSizePolicy, QFrame)
+from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLabel, QStackedWidget, QSizePolicy, QTableWidget, QTableWidgetItem)
 from PyQt6.QtGui import QFont, QAction
-from PyQt6.QtCore import Qt
+import nmap
+from PyQt6.QtCore import Qt, QThread, pyqtSignal
 
-"""
-    Para ejecutar el codigo nesecitaras las librerias
-    pip install PyQt6    
-"""
-
+dispositivos = []  
 estado = "beta"
+
 class OptiNet(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle(f"OptiNet -- {estado}")
-        self.setMinimumSize(1280,800)
+        self.setMinimumSize(1280, 800)
         self.UI()
         self.show()
     def UI(self):
         menu = self.menuBar()
         menu.setStyleSheet("background-color: black;")
         
-        menu_config = menu.addMenu("Cofiguracion")
+        menu_config = menu.addMenu("Configuracion")
         menu_ayuda = menu.addMenu("Ayuda")
         
         exit_action = QAction("Salir", self)
         exit_action.triggered.connect(self.close)
         menu_config.addAction(exit_action)
         
-        ayuda_action = QAction("Soporte",self)
+        ayuda_action = QAction("Soporte", self)
         ayuda_action.triggered.connect(self.soporte)
         menu_ayuda.addAction(ayuda_action)
-
         main_widget = QWidget()
         main_layout = QHBoxLayout(main_widget)
         
         """barra principal"""
-        
         barra_lat = QVBoxLayout()
-        
         def Boton(text):
             boton = QPushButton(text)
-            boton.setFont(QFont("",16))
-            boton.setSizePolicy(QSizePolicy.Policy.Preferred,QSizePolicy.Policy.Preferred)
+            boton.setFont(QFont("", 16))
+            boton.setSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
             return boton
         
         boton_principal = Boton("Inicio")
@@ -72,7 +65,7 @@ class OptiNet(QMainWindow):
         self.pagina_reporte = self.reporte()
         self.pagina_left = self.reporte()
         
-        for pagina in [self.pagina_principal, self.pagina_trafico, self.pagina_dispositivos,self.pagina_wifi,self.pagina_ancho_banda, self.pagina_reporte,self.pagina_left]:
+        for pagina in [self.pagina_principal, self.pagina_trafico, self.pagina_dispositivos, self.pagina_wifi, self.pagina_ancho_banda, self.pagina_reporte, self.pagina_left]:
             self.stack.addWidget(pagina)
         
         boton_principal.clicked.connect(lambda: self.stack.setCurrentWidget(self.pagina_principal))
@@ -83,53 +76,100 @@ class OptiNet(QMainWindow):
         boton_reporte.clicked.connect(lambda: self.stack.setCurrentWidget(self.pagina_reporte))
         boton_left.clicked.connect(lambda: self.stack.setCurrentWidget(self.pagina_left))
         
-        main_layout.addLayout(barra_lat,1)
-        main_layout.addWidget(self.stack,6)
-        
+        main_layout.addLayout(barra_lat, 1)
+        main_layout.addWidget(self.stack, 6)
         
         self.setCentralWidget(main_widget)
-        
     def soporte(self):
         """abrir pagina de ayuda"""
-        pass 
+        pass
+    
     def trafico(self):
         pagina = QWidget()
-        lay1 = QVBoxLayout(pagina)
-        label = QLabel("")
-        lay1.addWidget(label)
+        layoutmain = QVBoxLayout(pagina)
+        label = QLabel("Información de tráfico")
+        layoutmain.addWidget(label)
         return pagina
-    
     def dispositivos(self):
+        self.label_estado = QLabel("Esperando actualización...")
         pagina = QWidget()
-        lay1 = QVBoxLayout(pagina)
-        label = QLabel("hola1")
-        lay1.addWidget(label)
+        lay1 = QHBoxLayout(pagina)
+        lay2 = QVBoxLayout()
+        self.tabla_dispositivos = QTableWidget(self)
+        self.tabla_dispositivos.setRowCount(23)
+        self.tabla_dispositivos.setColumnCount(2)
+        self.tabla_dispositivos.setColumnWidth(0,425)
+        self.tabla_dispositivos.setColumnWidth(1,425)
+        self.tabla_dispositivos.setHorizontalHeaderLabels(["IP", "MAC"])
+        boton_actualizacion = QPushButton("Actualizar")
+        boton_actualizacion.setMaximumSize(100, 100)
+        boton_actualizacion.clicked.connect(self.actualizar_tabla_dispositivos)
+        
+        lay2.addWidget(self.label_estado)
+        lay2.addWidget(boton_actualizacion)
+        
+        lay1.addWidget(self.tabla_dispositivos, 5)
+        lay1.addLayout(lay2, 3)
         return pagina
     def wifi(self):
         pagina = QWidget()
         lay1 = QVBoxLayout(pagina)
-        label = QLabel("hola2")
+        label = QLabel("Wi-Fi")
         lay1.addWidget(label)
         return pagina
     def ancho_banda(self):
         pagina = QWidget()
         lay1 = QVBoxLayout(pagina)
-        label = QLabel("hola3")
+        label = QLabel("Ancho de Banda")
         lay1.addWidget(label)
         return pagina
     def reporte(self):
         pagina = QWidget()
         lay1 = QVBoxLayout(pagina)
-        label = QLabel("hola4")
+        label = QLabel("Reportes")
         lay1.addWidget(label)
         return pagina
     def principal(self):
         pagina = QWidget()
         lay1 = QVBoxLayout(pagina)
-        label = QLabel("hola5")
+        label = QLabel("Página Principal")
         lay1.addWidget(label)
         return pagina
-    
+    def actualizar_tabla_dispositivos(self):
+        # Se actualiza el estado para mostrar "Cargando..."
+        self.label_estado.setText("Cargando...")
+        
+        # Creamos un thread para escanear los dispositivos y evitar congelar la GUI
+        self.thread = DispositivosThread()
+        self.thread.dispositivos_actualizados.connect(self.actualizar_tabla)
+        self.thread.start()
+    def actualizar_tabla(self, dispositivos):
+        self.tabla_dispositivos.setRowCount(0)
+        for dispositivo in dispositivos:
+            row_position = self.tabla_dispositivos.rowCount()
+            self.tabla_dispositivos.insertRow(row_position)
+            self.tabla_dispositivos.setItem(row_position, 0, QTableWidgetItem(dispositivo[0]))
+            self.tabla_dispositivos.setItem(row_position, 1, QTableWidgetItem(dispositivo[1]))
+        
+        self.label_estado.setText("Actualizado")
+class DispositivosThread(QThread):
+    dispositivos_actualizados = pyqtSignal(list)
+    def run(self):
+        dispositivos = Dispositivos.escanear_dispositivos("192.168.18.0/24")
+        #dispositivos += Dispositivos.escanear_dispositivos("192.168.137.0/24")
+        self.dispositivos_actualizados.emit(dispositivos)
+class Dispositivos:
+    @staticmethod
+    def escanear_dispositivos(red="192.168.18.0/24"):
+        nm = nmap.PortScanner()
+        print(f"Escaneando la red {red}...")
+        nm.scan(hosts=red, arguments='-sn')
+        dispositivos = []
+        for host in nm.all_hosts():
+            ip = host
+            mac = nm[host]['addresses'].get('mac', 'MAC no disponible')
+            dispositivos.append([ip, mac])
+        return dispositivos
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     ventana = OptiNet()
