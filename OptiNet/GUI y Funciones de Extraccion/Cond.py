bc676fd01cac0faff255e338b2e8c10b75e01c7b
__link__ <68714207+linkold@users.noreply.github.com>
2025-06-13 11:14:28 -0500

    Delete OptiNet/GUI y Funciones de Extraccion directory

--- OptiNet/GUI y Funciones de Extraccion/Cond.py
+++
@@ -1,6 +0,0 @@
-from PyQt6.QtWidgets import QMessageBox
-import platform
-if platform.system() == "Windows":
-    from Funciones_windows import Datos, Control_Parental, datos_variantes
-else:
-    from Funciones_linux import Datos, Control_Parental, datos_variantes
